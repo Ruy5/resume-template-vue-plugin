a1a158ea36ff0de62d42b085b751e481d1fa0a60
Ruy5 <snake.snakeyy@gmail.com>
2024-06-19 20:41:20 +0800

    vite打包局部导出

--- build.js
+++ build.js
@@ -1,9 +1,6 @@
 import { build } from "vite";
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import { resolve, dirname } from 'path'
-import { fileURLToPath } from 'url'
 
 const components = ["", "A", "B"];
 
@@ -11,10 +8,7 @@ const librarys = components.map((name) => {
   return {
     entry: `./packages/resume-template/index${name}.js`,
     name: `ru-resume-template${name}`,
-    // formats: ['umd'],
-    
     fileName: (format) => {
-        console.log("******", format)
         return `ru-resume-template${name}.${format}.js`
     } 
   };
